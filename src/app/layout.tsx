cbc48cbe2e32d8484ade08cab4fb3f38d27495ec
abhave1 <abhaveabhilash@gmail.com>
2025-06-20 22:33:12 -0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Favicon */}
-        <link rel="icon" type="image/svg+xml" href="/vercel.svg" />
+        <link rel="icon" type="image/svg+xml" href="/white-circle.svg" />
         {/* Viewport */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         {/* Theme color */}
